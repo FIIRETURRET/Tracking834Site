484949d1268ecfb8724d96e908939d17371c6640
FIIRETURRET <brandonjoutras@gmail.com>
2019-10-03 14:38:59 -0500

    Add files via upload

--- tracker834/entries/admin.py
+++ tracker834/entries/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from .models import Entry834
 
 # Register your models here.
+class Entry834Admin(admin.ModelAdmin):
+    list_display = ('groupnum', 'name', 'assigned', 'renewal', 'fileType', 'testFileName', 'testDate', 'approvedWho', 'prodFileName', 'prodDate', 'analyticsVerified', 'approvedWhoTwo')
 
-admin.site.register(Entry834)
+admin.site.register(Entry834, Entry834Admin)
