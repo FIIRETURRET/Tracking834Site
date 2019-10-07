fb201e05900f5bab38f714cef81cb9cce6f80acb
FIIRETURRET <brandonjoutras@gmail.com>
2019-10-07 01:28:07 +0000

    Fixed up the detail view

--- tracker834/entries/models.py
+++ tracker834/entries/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.forms import ModelForm
-
 # Create your models here.
 
 class Query(models.Model):
@@ -9,21 +7,13 @@ class Query(models.Model):
     subfolder                   = models.CharField(max_length=100, blank=True, null=True)
     subfolder2                  = models.CharField(max_length=100, blank=True, null=True, default=None)
     queryUsedFor                = models.CharField(max_length=500, blank=True, null=True)
-    queryName                   = models.CharField(max_length=100)
+    queryName                   = models.CharField(max_length=100, unique=True)
     files                       = models.CharField(max_length=100, blank=True, null=True)
     convertYN                   = models.BooleanField(default=False)
     conversionStartDate         = models.DateField('Conversion Start Date', blank=True, null=True)
     assignedTo                  = models.CharField(max_length=100, blank=True, null=True)
     conversionCompletionDate    = models.DateField('Conversion Completion Date', blank=True, null=True)
     newNameInBI                 = models.CharField(max_length=100, blank=True, null=True)
-
-    def get_all_field_names(self):
-        return Query._meta.fields()
-
-    def formatNoneType(inString):
-        if inString is None:
-            return ''
-        return str(inString)
 
     def __str__(self):
         return  Query.formatNoneType(self.queryName) + " | " + Query.formatNoneType(self.assignedTo) + " | " + Query.formatNoneType(self.convertYN) + " | " + Query.formatNoneType(self.queryUsedFor)
--- tracker834/entries/views.py
+++ tracker834/entries/views.py
@@ -16,14 +16,14 @@ def index(request):
         'query_list': query_list,
     }
     return render(request, 'entries/index.html', context)
-    
-    
+
+
 def detail(request, query_id):
     query = get_object_or_404(Query, pk=query_id)
     data = serializers.serialize( "python", Query.objects.all() )
     return render(request, 'entries/detail.html', {'query': query, 'data': data})
-    
-    
+
+
 def export_entries_csv(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="entries.csv"'
@@ -36,26 +36,26 @@ def export_entries_csv(request):
         writer.writerow(entry)
 
     return response
-    
-# Makes it so only super users can access this    
+
+# Makes it so only super users can access this
 @permission_required('admin.can_add_log_entry')
 def csv_upload(request):
     template = 'entries/csv_upload.html'
-    
+
     prompt = {
         'order': 'order of CVS should be'
     }
-    
+
     if request.method=="GET":
         return render(request, template, prompt)
-    
+
     # Get the file
     csv_file = request.FILES['file']
-    
+
     # Check to see if the file uploaded is .csv
     if not csv_file.name.endswith('.csv'):
         messages.error(request, 'This is not a .csv file')
-    
+
     data_set = csv_file.read().decode('UTF-8')
     io_string = io.StringIO(data_set)
     # Skip the header
